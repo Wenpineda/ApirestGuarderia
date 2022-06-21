1aaefc0999c5b30237a7efb9ca7c622a2d788edf
Wenpineda <cp0428032021@unab.edu.sv>
2022-06-20 19:03:56 -0600

    APIRest-Guarderia

---
+++ ApiWe-Guarderia-main/WebAPI-Guarderia/WebAPI-Guarderia/Controllers/niñosController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace WebAPI_Guarderia.Controllers
+{
+    public class niñosController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        // GET: Producto
+        public IHttpActionResult Add()
+        {
+            using (Models.BDDguarderiaEntities db = new Models.BDDguarderiaEntities())
+            {
+
+                var oNiños = new Models.niños();
+                oNiños.nombre = "Jose";
+                oNiños.apellido = "Hernandez";
+                oNiños.edad = 9;
+                oNiños.sexo = "Masculino";
+                db.niños.Add(oNiños);
+                db.SaveChanges();
+            }
+            return Ok("Hecho");
+        }
+    }
+
+}
